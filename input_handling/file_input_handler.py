5ae5925ae4b9de46a1bdd8796ad7a9e9aa695bd4
Piotr S <pit56482@gmail.com>
2025-08-16 18:36:30 +0200

    Fix improper dataset loading

--- input_handling/file_input_handler.py
+++ input_handling/file_input_handler.py
@@ -18,10 +18,10 @@ class FileInputHandler:
             datasets = f.read().splitlines()
         # remove empty strings
         self._dataset_strings = list(filter(None, datasets))
-
-        d_object = DatasetObject()
+        
         errors = []
         for i, dataset in enumerate(self._dataset_strings):
+            d_object = DatasetObject()
             try:
                 d_object.from_string(dataset)
                 self._dataset_objects.append(d_object)
